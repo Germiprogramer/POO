b93534f60825965729e96814bca6d7c907a4b9aa
Germiprogramer <llorentemunozgerman@gmail.com>
2022-03-11 16:38:29 +0100

    commit

--- LO QUE VOY APRENDIENDO.py
+++ LO QUE VOY APRENDIENDO.py
@@ -1,5 +1,17 @@
 class pokemon:
     class pikachu:
         potencia = 20
+        vida = 60
+        def atacar(potencia):
+            daño = potencia
+            return daño
+    class charmander:
+        potencia = 25
+        vida = 50
+        def atacar(potencia):
+            daño = potencia
+            return daño
 
-print(pokemon.pikachu.potencia)
+vidanueva = pokemon.charmander.vida - pokemon.pikachu.potencia
+
+print(vidanueva)
